006e8eba759ce4371ed844ca0d769095b1db71db
Jorge Garcia <jorgealfonsogarcia@gmail.com>
2022-08-29 08:53:24 -0500

    :sparkles: (feat) Added Customer model and service.

--- src/main/java/co/edu/uan/sweng/architecture/devops/cd/model/entity/Customer.java
+++ src/main/java/co/edu/uan/sweng/architecture/devops/cd/model/entity/Customer.java
@@ -18,5 +18,130 @@
 
 package co.edu.uan.sweng.architecture.devops.cd.model.entity;
 
-public class Customer {
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serial;
+import java.io.Serializable;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+public class Customer implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 2133356295598671120L;
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String cellphoneNumber;
+    private String city;
+    private String state;
+    private String country;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCellphoneNumber() {
+        return cellphoneNumber;
+    }
+
+    public void setCellphoneNumber(String cellphoneNumber) {
+        this.cellphoneNumber = cellphoneNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        final var customer = (Customer) o;
+        return new EqualsBuilder().append(id, customer.id).append(email, customer.email)
+                .append(firstName, customer.firstName).append(lastName, customer.lastName)
+                .append(address, customer.address).append(cellphoneNumber, customer.cellphoneNumber)
+                .append(city, customer.city).append(state, customer.state).append(country, customer.country).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        final var initialOddNumber = 17;
+        final var multiplierOddNumber = 37;
+        return new HashCodeBuilder(initialOddNumber, multiplierOddNumber).append(id).append(email).append(firstName)
+                .append(lastName).append(address).append(cellphoneNumber).append(city).append(state).append(country)
+                .toHashCode();
+    }
 }
---
+++ src/main/java/co/edu/uan/sweng/architecture/devops/cd/model/service/CustomerService.java
@@ -0,0 +1,56 @@
+/*
+ *     uan-spec-sweng-architecture-2-expo-01-devops-continuous-deployment-webapp
+ *     Copyright (C) 2022  Garcia, J; Poveda, D; UAN
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package co.edu.uan.sweng.architecture.devops.cd.model.service;
+
+import co.edu.uan.sweng.architecture.devops.cd.model.entity.Customer;
+import co.edu.uan.sweng.architecture.devops.cd.persistence.repository.CustomerRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Slf4j
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    public Customer save(Customer customer) {
+        return customerRepository.saveAndFlush(customer);
+    }
+
+    public Optional<Customer> findById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    public void deleteById(Long id) {
+        customerRepository.deleteById(id);
+    }
+}
