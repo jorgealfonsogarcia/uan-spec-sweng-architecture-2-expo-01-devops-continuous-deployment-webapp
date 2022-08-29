54f958999de51b29f3da8cc523e7f7ac35dc6baa
Jorge Garcia <jorgealfonsogarcia@gmail.com>
2022-08-29 09:25:52 -0500

    :sparkles: (feat) Added UI base for Index.

---
+++ src/main/java/co/edu/uan/sweng/architecture/devops/cd/controller/ui/IndexController.java
@@ -0,0 +1,32 @@
+/*
+ *     uan-spec-sweng-architecture-2-expo-01-devops-continuous-deployment-webapp
+ *     Copyright (C) 2022  Garcia, J; Poveda, D; UAN
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package co.edu.uan.sweng.architecture.devops.cd.controller.ui;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping
+    public String index() {
+        return "index";
+    }
+}
